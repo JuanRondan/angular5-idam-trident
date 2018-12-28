e735b0f77344b0cf6a04b47440a14855a51b5f2c
Juan Rondán <juan.m.rondan@pwc.com>
2018-12-28 12:04:28 -0300

    add trident and user roles component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { UsersComponent } from './users/users.component';
+import { RolesComponent } from './roles/roles.component';
 
 import { Angular2IdamModule } from '@pa-util/angular2-idam';
 import { GLOBALS } from './idam/global';
 import { AuthInterceptorService } from './idam/interceptor';
-import { environment } from '../environments/environment';
+import { TridentModule } from '@pa-util/trident-rolemanagement';
 
-const appRoutes : Routes = [];
+const appRoutes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'roles', component: RolesComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    RolesComponent
   ],
   imports: [
     BrowserModule,
@@ -22,12 +30,18 @@ const appRoutes : Routes = [];
     Angular2IdamModule.forRoot({
       discoveryUrl: environment.discoveryUrl,
       redirect: environment.redirect,
-      clientId: environment.clientId
+      clientId: environment.clientId,
+      tridentUrl: environment.tridentUrl,
+      autoRefresh: false,
+      timeToRefresh: 120
+    }),
+    TridentModule.forRoot({
+      tridentUrl: environment.tridentUrl
     }),
     RouterModule.forRoot(appRoutes, { enableTracing: environment.production }),
   ],
   providers: [
-    { provide: GLOBALS, multi: false, useValue: {}}, //INCLUDE THIS IF YOU ARE CREATING AN INJECTION TOKEN (shown below)
+    { provide: GLOBALS, multi: false, useValue: {} }, //INCLUDE THIS IF YOU ARE CREATING AN INJECTION TOKEN (shown below)
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }, //INCLUDE THIS IF YOU ARE USING AN INTERCEPTOR (angular version 4.3 and above, shown below)
   ],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,5 +9,6 @@ export const environment = {
   // Idam endpoints 
   discoveryUrl: 'https://fedsvc-stage.pwc.com/ofisids/api/discovery',
   redirect: 'http://localhost:55000/', // You can also pass an array of redirect urls like ['http://localhost:55000/', 'http://localhost:4200'],
-  clientId: 'urn:oauth2:test:s'
+  clientId: 'urn:oauth2:test:s',
+  tridentUrl: 'https://trident-api-dev.azurewebsites.net'
 };
